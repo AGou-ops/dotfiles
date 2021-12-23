b05bc27246cee31583e3dd1dc0ef655c33b168b2
AGou-ops <suofeiyaxx@gmail.com>
2021-12-23 20:20:02 +0800

    update some plugins and settings

--- neovim-with-lua/lua/AGou/init.lua
+++ neovim-with-lua/lua/AGou/init.lua
@@ -4,5 +4,17 @@ require("bufferline").setup{}
 -- 好像没什么卵用啊.
 -- require'lspconfig'.bashls.setup{} -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#bashls
 require('AGou.nvim-tree')
-
-
+-- ----------------------------------------------------------------------
+-- full hotkeys usage: https://github.com/numToStr/Comment.nvim#-usage
+-- gcc: 当前行行注释
+-- gcb: 当前行块注释
+-- [count]gcc/gcb: 多行块或行注释
+-- `gco` - Insert comment to the next line and enters INSERT mode
+-- `gcO` - Insert comment to the previous line and enters INSERT mode
+-- `gcA` - Insert comment to end of the current line and enters INSERT mode
+require('Comment').setup{
+        mappings = {
+        extended = true,
+    },
+}
+-- ----------------------------------------------------------------------
--- neovim-with-lua/lua/AGou/nvim-tree.lua
+++ neovim-with-lua/lua/AGou/nvim-tree.lua
@@ -1,5 +1,6 @@
 -- following options are the default
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
+local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 require'nvim-tree'.setup {
   disable_netrw       = true,
   hijack_netrw        = true,
@@ -48,7 +49,12 @@ require'nvim-tree'.setup {
     auto_resize = true,
     mappings = {
       custom_only = false,
-      list = {}
+      list = {
+          { key = {"<CR>", "<2-LeftMouse>"}, cb = tree_cb("edit") },
+          { key = "v",                        cb = tree_cb("vsplit") },
+          { key = "s",                        cb = tree_cb("split") },
+          { key = "o",                            cb = tree_cb("system_open") },
+      }
     },
     number = true,
     relativenumber = false,
@@ -58,4 +64,43 @@ require'nvim-tree'.setup {
     cmd = "trash",
     require_confirm = true
   }
+}
+
+
+local tree_cb = require'nvim-tree.config'.nvim_tree_callback
+-- default mappings
+local list = {
+  { key = {"<CR>", "<2-LeftMouse>"}, cb = tree_cb("edit") },
+  { key = {"<2-RightMouse>", "<C-]>"},    cb = tree_cb("cd") },
+  { key = "v",                        cb = tree_cb("vsplit") },
+  { key = "s",                        cb = tree_cb("split") },
+  { key = "<C-t>",                        cb = tree_cb("tabnew") },
+  { key = "<",                            cb = tree_cb("prev_sibling") },
+  { key = ">",                            cb = tree_cb("next_sibling") },
+  { key = "P",                            cb = tree_cb("parent_node") },
+  { key = "<BS>",                         cb = tree_cb("close_node") },
+  { key = "<S-CR>",                       cb = tree_cb("close_node") },
+  { key = "<Tab>",                        cb = tree_cb("preview") },
+  { key = "K",                            cb = tree_cb("first_sibling") },
+  { key = "J",                            cb = tree_cb("last_sibling") },
+  { key = "I",                            cb = tree_cb("toggle_ignored") },
+  { key = "H",                            cb = tree_cb("toggle_dotfiles") },
+  { key = "R",                            cb = tree_cb("refresh") },
+  { key = "a",                            cb = tree_cb("create") },
+  { key = "d",                            cb = tree_cb("remove") },
+  { key = "D",                            cb = tree_cb("trash") },
+  { key = "r",                            cb = tree_cb("rename") },
+  { key = "<C-r>",                        cb = tree_cb("full_rename") },
+  { key = "x",                            cb = tree_cb("cut") },
+  { key = "c",                            cb = tree_cb("copy") },
+  { key = "p",                            cb = tree_cb("paste") },
+  { key = "y",                            cb = tree_cb("copy_name") },
+  { key = "Y",                            cb = tree_cb("copy_path") },
+  { key = "gy",                           cb = tree_cb("copy_absolute_path") },
+  { key = "[c",                           cb = tree_cb("prev_git_item") },
+  { key = "]c",                           cb = tree_cb("next_git_item") },
+  { key = "-",                            cb = tree_cb("dir_up") },
+  { key = "o",                            cb = tree_cb("system_open") },
+  { key = "q",                            cb = tree_cb("close") },
+  { key = "g?",                           cb = tree_cb("toggle_help") },
 }
