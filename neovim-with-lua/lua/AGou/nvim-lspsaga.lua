86c76851d6e8e04a3c212d63f4c71ebaee929639
AGou-ops <suofeiyaxx@gmail.com>
2022-05-20 09:46:51 +0800

    Enable code_action_prompt.

--- neovim-with-lua/lua/AGou/nvim-lspsaga.lua
+++ neovim-with-lua/lua/AGou/nvim-lspsaga.lua
@@ -11,10 +11,10 @@ lspsaga.setup { -- defaults ...
     -- code action title icon
     code_action_icon = " ",
     code_action_prompt = {
-        enable = false,
+        enable = true,
         sign = false,
         sign_priority = 40,
-        virtual_text = false,
+        virtual_text = true,
     },
     finder_definition_icon = "  ",
     finder_reference_icon = "  ",
