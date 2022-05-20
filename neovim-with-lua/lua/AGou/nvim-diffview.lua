2f23a016792f8a8e19a3c7751b3cf2a1d24541a1
AGou-ops <suofeiyaxx@gmail.com>
2022-05-20 09:46:23 +0800

    Replace some DEPRECATED configuration.

--- neovim-with-lua/lua/AGou/nvim-diffview.lua
+++ neovim-with-lua/lua/AGou/nvim-diffview.lua
@@ -14,19 +14,17 @@ require("diffview").setup({
     fold_open = "",
   },
   file_panel = {
-    position = "left", -- One of 'left', 'right', 'top', 'bottom'
-    width = 35, -- Only applies when position is 'left' or 'right'
-    height = 10, -- Only applies when position is 'top' or 'bottom'
     listing_style = "tree", -- One of 'list' or 'tree'
     tree_options = { -- Only applies when listing_style is 'tree'
       flatten_dirs = true, -- Flatten dirs that only contain one single dir
       folder_statuses = "only_folded", -- One of 'never', 'only_folded' or 'always'.
     },
+    win_config = { -- See ':h diffview-config-win_config'
+      position = "left",
+      width = 35,
+    },
   },
   file_history_panel = {
-    position = "bottom",
-    width = 35,
-    height = 16,
     log_options = {
       max_count = 256, -- Limit the number of commits
       follow = false, -- Follow renames (only for single file)
@@ -34,6 +32,10 @@ require("diffview").setup({
       merges = false, -- List only merge commits
       no_merges = false, -- List no merge commits
       reverse = false, -- List commits in reverse order
+    },
+    win_config = { -- See ':h diffview-config-win_config'
+      position = "bottom",
+      height = 16,
     },
   },
   default_args = { -- Default args prepended to the arg-list for the listed commands
