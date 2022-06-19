0c112860c9918ad5000e215fe157623ac2b5b6cd
AGou-ops <suofeiyaxx@gmail.com>
2022-06-19 09:38:32 +0800

    Nothing changed.

--- neovim-with-lua/lua/AGou/nvim-diffview.lua
+++ neovim-with-lua/lua/AGou/nvim-diffview.lua
@@ -25,14 +25,14 @@ require("diffview").setup({
     },
   },
   file_history_panel = {
-    log_options = {
-      max_count = 256, -- Limit the number of commits
-      follow = false, -- Follow renames (only for single file)
-      all = false, -- Include all refs under 'refs/' including HEAD
-      merges = false, -- List only merge commits
-      no_merges = false, -- List no merge commits
-      reverse = false, -- List commits in reverse order
-    },
+    -- log_options = {
+    --   max_count = 256, -- Limit the number of commits
+    --   follow = false, -- Follow renames (only for single file)
+    --   all = false, -- Include all refs under 'refs/' including HEAD
+    --   merges = false, -- List only merge commits
+    --   no_merges = false, -- List no merge commits
+    --   reverse = false, -- List commits in reverse order
+    -- },
     win_config = { -- See ':h diffview-config-win_config'
       position = "bottom",
       height = 16,
