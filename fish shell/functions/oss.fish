96d697a2d5ca2b339779e228d337940186bbd757
AGou-ops <suofeiyaxx@gmail.com>
2022-08-04 09:48:27 +0800

    Remove.

--- fish shell/functions/oss.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 0
-function oss --description 'alias oss=ossutil64'
-	ossutil64  $argv;
-end
--- fish shell/functions/ssr.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 0
-function ssr --description 'alias ssr=/home/suofeiya/Shadowsocks-Qt5-3.0.1-x86_64.AppImage'
-	/home/suofeiya/Shadowsocks-Qt5-3.0.1-x86_64.AppImage  $argv;
-end
