80e738c9d2d5e63c8917ed634d0fa9af536bad23
AGou-ops <suofeiyaxx@gmail.com>
2022-08-06 23:58:50 +0800

    Update.

--- neovim-with-lua/lua/AGou/nvim-surround.lua
+++ neovim-with-lua/lua/AGou/nvim-surround.lua
@@ -1,36 +1,20 @@
 require("nvim-surround").setup({
-    keymaps = { -- vim-surround style keymaps
-        -- insert = "ys",
-        visual = "Q",
-        delete = "ds",
-        change = "cs",
-    },
-    delimiters = {
-        pairs = {
-            ["("] = { "( ", " )" },
-            [")"] = { "(", ")" },
-            ["{"] = { "{ ", " }" },
-            ["}"] = { "{", "}" },
-            ["<"] = { "< ", " >" },
-            [">"] = { "<", ">" },
-            ["["] = { "[ ", " ]" },
-            ["]"] = { "[", "]" },
-            ["*"] = { "**", "**" },
-        },
-        separators = {
-            ["'"] = { "'", "'" },
-            ['"'] = { '"', '"' },
-            ["`"] = { "`", "`" },
-        },
-        HTML = {
-            ["t"] = true, -- Use "t" for HTML-style mappings
-        },
-        aliases = {
-            ["a"] = ">", -- Single character aliases apply everywhere
-            ["b"] = ")",
-            ["B"] = "}",
-            ["r"] = "]",
-            ["q"] = { '"', "'", "`" }, -- Table aliases only apply for changes/deletions
-        },
-    }
+  -- Configuration here, or leave empty to use defaults
+  keymaps = {
+    insert = "<C-g>s",
+    insert_line = "<C-g>S",
+    normal = "ys",
+    normal_cur = "yss",
+    normal_line = "yS",
+    normal_cur_line = "ySS",
+    visual = "Q",
+    visual_line = "gS",
+    delete = "ds",
+    change = "cs",
+  },
 })
+
+-- sample Text
+--
+-- aaaaaaaa        bbbb
+-- cccccccc        dddd
