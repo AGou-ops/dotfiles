2d68f5427f671057859eab78af7ce9e42fa9a037
AGou-ops <suofeiyaxx@gmail.com>
2022-08-20 20:34:40 +0800

    Update at 2022-08-20 20:34:38.

--- lua/AGou/nvim-telescope.lua
+++ lua/AGou/nvim-telescope.lua
@@ -14,7 +14,48 @@ telescope.setup {
 
     prompt_prefix = "> ",
     selection_caret = " ",
-    path_display = { "smart" },
+    -- path_display = { "smart" },
+    entry_prefix = "  ",
+    initial_mode = "insert",
+    selection_strategy = "reset",
+    sorting_strategy = "ascending",
+    layout_strategy = "horizontal",
+
+    layout_config = {
+      horizontal = {
+        prompt_position = "top",
+        preview_width = 0.55,
+        results_width = 0.8,
+      },
+      vertical = {
+        mirror = false,
+      },
+      width = 0.87,
+      height = 0.80,
+      preview_cutoff = 120,
+    },
+    winblend = 0,
+    -- border = {},
+    -- borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+    -- borderchars = {
+    --   prompt = { '▀', '▐', '▄', '▌', '▛', '▜', '▟', '▙' };
+    --   results = { '▀', '▐', '▄', '▌', '▛', '▜', '▟', '▙' };
+    --   preview = { '▀', '▐', '▄', '▌', '▛', '▜', '▟', '▙' };
+    -- },
+    -- borderchars = {
+    --   prompt = { '▀', '▐', '▄', '▌', '▛', '▜', '▟', '▙' };
+    --   results = { ' ', '▐', '▄', '▌', '▌', '▐', '▟', '▙' };
+    --   preview = { '▀', '▐', '▄', '▌', '▛', '▜', '▟', '▙' };
+    -- },
+    color_devicons = true,
+    -- use_less = true,
+    -- set_env = { ["COLORTERM"] = "gruvbox" }, -- default = nil,
+    -- file_sorter = require("telescope.sorters").get_fuzzy_file,
+    file_ignore_patterns = { "node_modules" },
+    -- generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+    path_display = { "truncate" },
+    dynamic_preview_title = true,
+    results_title = false,
 
     mappings = {
       i = {
