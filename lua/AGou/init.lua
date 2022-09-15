fa5253e9e7afe6697519588cb87edef5c4ce32dd
AGou-ops <suofeiyaxx@gmail.com>
2022-09-15 09:57:39 +0800

    Update at 2022-09-15 09:57:38.

--- lua/AGou/init.lua
+++ lua/AGou/init.lua
@@ -167,6 +167,28 @@ require('neogen').setup({
 
 })
 -- --------------------------------------
+--require('satellite').setup {
+--  current_only = false,
+--  winblend = 50,
+--  zindex = 40,
+--  excluded_filetypes = {},
+--  width = 2,
+--  handlers = {
+--    search = {
+--      enable = true,
+--    },
+--    diagnostic = {
+--      enable = true,
+--    },
+--    gitsigns = {
+--      enable = true,
+--    },
+--    marks = {
+--      enable = true,
+--      show_builtins = false, -- shows the builtin marks like [ ] < >
+--    },
+--  },
+--}
 -- --------------------------------------
 -- --------------------------------------
 -- ----------------------------------------------------------------------
--- lua/AGou/nvim-lspsaga.lua
+++ lua/AGou/nvim-lspsaga.lua
@@ -1,139 +1,103 @@
--- tami5 lspsaga
--- local lspsaga = require 'lspsaga'
--- lspsaga.setup { -- defaults ...
---     debug = false,
---     use_saga_diagnostic_sign = true,
---     -- diagnostic sign
---     error_sign = "",
---     warn_sign = "",
---     hint_sign = "",
---     infor_sign = "",
---     diagnostic_header_icon = "   ",
---     -- code action title icon
---     code_action_icon = " ",
---     code_action_prompt = {
---         enable = true,
---         sign = false,
---         sign_priority = 40,
---         virtual_text = true,
---     },
---     finder_definition_icon = "  ",
---     finder_reference_icon = "  ",
---     max_preview_lines = 10,
---     finder_action_keys = {
---         open = "o",
---         vsplit = "v",
---         split = "s",
---         quit = "q",
---         scroll_down = "<C-d>",
---         scroll_up = "<C-u>",
---     },
---     code_action_keys = {
---         quit = "q",
---         exec = "<CR>",
---     },
---     rename_action_keys = {
---         quit = "<C-c>",
---         exec = "<CR>",
---     },
---     definition_preview_icon = "  ",
---     border_style = "round",
---     rename_prompt_prefix = "➤",
---     rename_output_qflist = {
---         enable = false,
---         auto_open_qflist = false,
---     },
---     server_filetype_map = {},
---     diagnostic_prefix_format = "%d. ",
---     diagnostic_message_format = "%m %c",
---     highlight_prefix = true,
--- }
-
-
-
--- new
-
-local saga = require 'lspsaga'
+local saga = require("lspsaga")
 
 -- change the lsp symbol kind
 -- local kind = require('lspsaga.lspkind')
 
 saga.init_lsp_saga({
-    -- Options with default value
-    -- "single" | "double" | "rounded" | "bold" | "plus"
-    border_style = "rounded",
-    -- when cursor in saga window you config these to move
-    move_in_saga = { prev = '<C-p>',next = '<C-n>'},
-    -- Error, Warn, Info, Hint
-    -- use emoji like
-    -- { "🙀", "😿", "😾", "😺" }
-    -- or
-    -- { "😡", "😥", "😤", "😐" }
-    -- and diagnostic_header can be a function type
-    -- must return a string and when diagnostic_header
-    -- is function type it will have a param `entry`
-    -- entry is a table type has these filed
-    -- { bufnr, code, col, end_col, end_lnum, lnum, message, severity, source }
-    diagnostic_header = { " ", " ", " ", "ﴞ " },
-    -- show diagnostic source
-    show_diagnostic_source = true,
-    -- add bracket or something with diagnostic source, just have 2 elements
-    diagnostic_source_bracket = {},
-    -- use emoji lightbulb in default
-    code_action_icon = "💡",
-    -- if true can press number to execute the codeaction in codeaction window
-    code_action_num_shortcut = true,
-    -- same as nvim-lightbulb but async
-    code_action_lightbulb = {
-        enable = true,
-        sign = true,
-        sign_priority = 20,
-        virtual_text = false,
-    },
-    -- finder icons
-    finder_icons = {
-        def = '  ',
-        ref = '諭 ',
-        link = '  ',
-    },
-    -- preview lines of lsp_finder and definition preview
-    max_preview_lines = 10,
-    finder_action_keys = {
-        open = "o",
-        vsplit = "v",
-        split = "s",
-        tabe = "t",
-        quit = "q",
-        scroll_down = "<C-d>",
-        scroll_up = "<C-u>", -- quit can be a table
-    },
-    code_action_keys = {
-        quit = "q",
-        exec = "<CR>",
-    },
-    rename_action_quit = "<C-c>",
-    definition_preview_icon = "  ",
-    -- show symbols in winbar must nightly
-    symbol_in_winbar = {
-        in_custom = false,
-        enable = false,
-        separator = ' ',
-        show_file = true,
-        click_support = false,
-    },
-    -- show outline
-    show_outline = {
-        win_position = 'left',
-        -- set the special filetype in there which in left like nvimtree neotree defx
-        left_with = '',
-        win_width = 30,
-        auto_enter = true,
-        auto_preview = true,
-        virt_text = '┃',
-        jump_key = 'o',
-    },
-    -- if you don't use nvim-lspconfig you must pass your server name and
-    -- the related filetypes into this table
-    -- like server_filetype_map = { metals = { "sbt", "scala" } }
-    server_filetype_map = {},
+  -- Options with default value
+  -- "single" | "double" | "rounded" | "bold" | "plus"
+  border_style = "rounded",
+  --the range of 0 for fully opaque window (disabled) to 100 for fully
+  --transparent background. Values between 0-30 are typically most useful.
+  saga_winblend = 0,
+  -- when cursor in saga window you config these to move
+  move_in_saga = { prev = "<C-p>", next = "<C-n>" },
+  -- Error, Warn, Info, Hint
+  -- use emoji like
+  -- { "🙀", "😿", "😾", "😺" }
+  -- or
+  -- { "😡", "😥", "😤", "😐" }
+  -- and diagnostic_header can be a function type
+  -- must return a string and when diagnostic_header
+  -- is function type it will have a param `entry`
+  -- entry is a table type has these filed
+  -- { bufnr, code, col, end_col, end_lnum, lnum, message, severity, source }
+  diagnostic_header = { " ", " ", " ", "ﴞ " },
+  -- preview lines of lsp_finder and definition preview
+  max_preview_lines = 10,
+  -- use emoji lightbulb in default
+  code_action_icon = "💡",
+  -- if true can press number to execute the codeaction in codeaction window
+  code_action_num_shortcut = true,
+  -- same as nvim-lightbulb but async
+  code_action_lightbulb = {
+    enable = true,
+    enable_in_insert = false,
+    cache_code_action = true,
+    sign = true,
+    update_time = 150,
+    sign_priority = 20,
+    virtual_text = false,
+  },
+  -- finder icons
+  finder_icons = {
+    def = "  ",
+    ref = "諭 ",
+    link = "  ",
+  },
+  -- finder do lsp request timeout
+  -- if your project big enough or your server very slow
+  -- you may need to increase this value
+  finder_request_timeout = 1500,
+  -- set antoher colorscheme in preview window
+  -- finder_preview_hl_ns = 0,
+  finder_action_keys = {
+    open = "o",
+    vsplit = "v",
+    split = "s",
+    tabe = "t",
+    quit = "q",
+  },
+  code_action_keys = {
+    quit = "q",
+    exec = "<CR>",
+  },
+  definition_action_keys = {
+    edit = "<C-c>o",
+    vsplit = "<C-c>v",
+    split = "<C-c>i",
+    tabe = "<C-c>t",
+    quit = "q",
+  },
+  rename_action_quit = "<C-c>",
+  rename_in_select = true,
+  -- show symbols in winbar must nightly
+  symbol_in_winbar = {
+    in_custom = false,
+    enable = false,
+    separator = " ",
+    show_file = true,
+    click_support = false,
+  },
+  -- show outline
+  show_outline = {
+    win_position = "right",
+    --set special filetype win that outline window split.like NvimTree neotree
+    -- defx, db_ui
+    win_with = "",
+    win_width = 30,
+    auto_enter = true,
+    auto_preview = true,
+    virt_text = "┃",
+    jump_key = "o",
+    -- auto refresh when change buffer
+    auto_refresh = true,
+  },
+  -- custom lsp kind
+  -- usage { Field = 'color code'} or {Field = {your icon, your color code}}
+  custom_kind = {},
+  -- if you don't use nvim-lspconfig you must pass your server name and
+  -- the related filetypes into this table
+  -- like server_filetype_map = { metals = { "sbt", "scala" } }
+  server_filetype_map = {},
 })
