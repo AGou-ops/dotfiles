2a5455fc57e43346d6a1e7d4ce2d7af3e4115011
AGou-ops <suofeiyaxx@gmail.com>
2022-11-10 17:09:11 +0800

    Add space and eof chars for indent_blankline.

--- neovim-with-lua/lua/AGou/nvim-indent-blankline.lua
+++ neovim-with-lua/lua/AGou/nvim-indent-blankline.lua
@@ -4,6 +4,10 @@ if not ok then
     return
 end
 
+vim.opt.list = true
+vim.opt.listchars:append "space:⋅"
+vim.opt.listchars:append "eol:⤸"
+
 indent.setup {
     char = "│",
     space_char_blankline = " ",
