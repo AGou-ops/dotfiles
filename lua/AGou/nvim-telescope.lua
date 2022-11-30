9dc5b4ab9344ec62dbf3b1aef805a68c8c653859
AGou-ops <suofeiyaxx@gmail.com>
2022-11-30 10:10:19 +0800

    Update at 2022-11-30 10:10:18.

--- lua/AGou/nvim-telescope.lua
+++ lua/AGou/nvim-telescope.lua
@@ -23,7 +23,7 @@ telescope.setup {
 
     layout_config = {
       horizontal = {
-        prompt_position = "top",
+        prompt_position = "bottom",
         preview_width = 0.55,
         results_width = 0.8,
       },
