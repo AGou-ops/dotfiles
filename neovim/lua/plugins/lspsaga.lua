84a3b081063c1ce901484b875dc5d3ed3865b0c5
AGou-ops <suofeiyaxx@gmail.com>
2023-02-01 15:55:05 +0800

    Update lspsaga config to latest version.

--- neovim/lua/plugins/lspsaga.lua
+++ neovim/lua/plugins/lspsaga.lua
@@ -1,101 +1,103 @@
-local M = {"glepnir/lspsaga.nvim", ft = {"go", "lua"}, commit = "b7b4777"}
+local M = {
+    "glepnir/lspsaga.nvim",
+    ft = {"go", "lua"}
+    -- commit = "b7b4777"
+}
 
 function M.config()
 
-    require("lspsaga").init_lsp_saga({
-        -- Options with default value
-        -- "single" | "double" | "rounded" | "bold" | "plus"
-        border_style = "rounded",
-        -- the range of 0 for fully opaque window (disabled) to 100 for fully
-        -- transparent background. Values between 0-30 are typically most useful.
-        saga_winblend = 0,
-        -- when cursor in saga window you config these to move
-        move_in_saga = {prev = "<C-p>", next = "<C-n>"},
-        -- Error, Warn, Info, Hint
-        -- use emoji like
-        -- { "🙀", "😿", "😾", "😺" }
-        -- or
-        -- { "😡", "😥", "😤", "😐" }
-        -- and diagnostic_header can be a function type
-        -- must return a string and when diagnostic_header
-        -- is function type it will have a param `entry`
-        -- entry is a table type has these filed
-        -- { bufnr, code, col, end_col, end_lnum, lnum, message, severity, source }
-        diagnostic_header = {" ", " ", " ", "ﴞ "},
-        -- preview lines of lsp_finder and definition preview
-        max_preview_lines = 10,
-        -- use emoji lightbulb in default
-        code_action_icon = "💡",
-        -- if true can press number to execute the codeaction in codeaction window
-        code_action_num_shortcut = true,
-        -- same as nvim-lightbulb but async
-        code_action_lightbulb = {
+    require("lspsaga").setup({
+        ui = {
+            border = 'rounded',
+            title = true,
+            winblend = 0,
+            expand = '',
+            collapse = '',
+            code_action = '💡',
+            diagnostic = '🐞',
+            incoming = ' ',
+            outgoing = ' ',
+            hover = ' ',
+            kind = {}
+        },
+        diagnostic = {
+            show_code_action = true,
+            show_source = true,
+            jump_num_shortcut = true,
+            max_width = 0.7,
+            custom_fix = nil,
+            custom_msg = nil,
+            text_hl_follow = false,
+            keys = {exec_action = 'o', quit = 'q', go_action = 'g'}
+        },
+        code_action = {num_shortcut = true, keys = {quit = 'q', exec = '<CR>'}},
+        lightbulb = {
             enable = true,
-            enable_in_insert = false,
-            cache_code_action = true,
+            enable_in_insert = true,
+            -- cache_code_action = true,
             sign = true,
-            update_time = 150,
-            sign_priority = 20,
+            sign_priority = 40,
             virtual_text = false
         },
-        -- finder icons
-        finder_icons = {def = "  ", ref = "諭 ", link = "  "},
-        -- finder do lsp request timeout
-        -- if your project big enough or your server very slow
-        -- you may need to increase this value
-        finder_request_timeout = 1500,
-        -- set antoher colorscheme in preview window
-        -- finder_preview_hl_ns = 0,
-        finder_action_keys = {
-            open = "o",
-            vsplit = "v",
-            split = "s",
-            tabe = "t",
-            quit = "q"
+        preview = {lines_above = 0, lines_below = 10},
+        scroll_preview = {scroll_down = '<C-f>', scroll_up = '<C-b>'},
+        request_timeout = 2000,
+        finder = {
+            jump_to = 'p',
+            edit = {'o', '<CR>'},
+            vsplit = 'v',
+            split = 's',
+            tabe = 't',
+            quit = {'q', '<ESC>'}
         },
-        code_action_keys = {quit = "q", exec = "<CR>"},
-        definition_action_keys = {
-            edit = "<C-c>o",
-            vsplit = "<C-c>v",
-            split = "<C-c>i",
-            tabe = "<C-c>t",
-            quit = "q"
+        definition = {
+            edit = '<C-c>o',
+            vsplit = '<C-c>v',
+            split = '<C-c>i',
+            tabe = '<C-c>t',
+            quit = 'q',
+            close = '<Esc>'
         },
-        rename_action_quit = "<C-c>",
-        rename_in_select = true,
-        -- show symbols in winbar must nightly
+        rename = {
+            quit = '<C-c>',
+            exec = '<CR>',
+            mark = 'x',
+            confirm = '<CR>',
+            in_select = true
+        },
         symbol_in_winbar = {
-            in_custom = false,
             enable = false,
             separator = ' ',
+            hide_keyword = true,
             show_file = true,
-            -- define how to customize filename, eg: %:., %
-            -- if not set, use default value `%:t`
-            -- more information see `vim.fn.expand` or `expand`
-            -- ## only valid after set `show_file = true`
-            file_formatter = "",
-            click_support = false
+            folder_level = 2,
+            respect_root = false,
+            color_mode = true
         },
-        -- show outline
-        show_outline = {
-            win_position = "right",
-            -- set special filetype win that outline window split.like NvimTree neotree
-            -- defx, db_ui
-            win_with = "",
+        outline = {
+            win_position = 'right',
+            win_with = '',
             win_width = 30,
-            auto_enter = true,
+            show_detail = true,
             auto_preview = true,
-            virt_text = "┃",
-            jump_key = "o",
-            -- auto refresh when change buffer
-            auto_refresh = true
+            auto_refresh = true,
+            auto_close = true,
+            custom_sort = nil,
+            keys = {jump = 'o', expand_collapse = 'u', quit = 'q'}
         },
-        -- custom lsp kind
-        -- usage { Field = 'color code'} or {Field = {your icon, your color code}}
-        custom_kind = {},
-        -- if you don't use nvim-lspconfig you must pass your server name and
-        -- the related filetypes into this table
-        -- like server_filetype_map = { metals = { "sbt", "scala" } }
+        callhierarchy = {
+            show_detail = false,
+            keys = {
+                edit = 'e',
+                vsplit = 's',
+                split = 'i',
+                tabe = 't',
+                jump = 'o',
+                quit = 'q',
+                expand_collapse = 'u'
+            }
+        },
+        beacon = {enable = true, frequency = 7},
         server_filetype_map = {}
     })
 end
