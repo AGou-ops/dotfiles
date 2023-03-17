5441799e6ea8e23de2733bb66072bdd8766f4c80
AGou-ops <suofeiyaxx@gmail.com>
2023-03-17 11:21:43 +0800

    Just update config.

--- neovim/lua/plugins/toggleterm.lua
+++ neovim/lua/plugins/toggleterm.lua
@@ -1,91 +1,91 @@
 local M = {
-	"akinsho/toggleterm.nvim",
-	event = "VeryLazy",
+    'akinsho/toggleterm.nvim',
+    event = 'VeryLazy',
 }
 
 function M.config()
-	local status_ok, toggleterm = pcall(require, "toggleterm")
-	if not status_ok then
-		return
-	end
+    local status_ok, toggleterm = pcall(require, 'toggleterm')
+    if not status_ok then
+        return
+    end
 
-	toggleterm.setup({
-		hide_numbers = true,
-		shade_filetypes = {},
-		shade_terminals = true,
-		shading_factor = 2,
-		start_in_insert = true,
-		insert_mappings = true,
-		persist_size = true,
-		direction = "float",
-		close_on_exit = true,
-		shell = vim.o.shell,
-		float_opts = {
-			border = "double", -- 'single' | 'double' | 'shadow' | 'curved' | ... other options supported by win open
-			winblend = 0,
-			highlights = {
-				border = "Normal",
-				background = "Normal",
-			},
-		},
-		winbar = {
-			enabled = true,
-			name_formatter = function(term) --  term: Terminal
-				return term.name
-			end
-		},
-	})
+    toggleterm.setup({
+        hide_numbers = true,
+        shade_filetypes = {},
+        shade_terminals = true,
+        shading_factor = 2,
+        start_in_insert = true,
+        insert_mappings = true,
+        persist_size = true,
+        direction = 'float',
+        close_on_exit = true,
+        shell = vim.o.shell,
+        float_opts = {
+            border = 'double', -- 'single' | 'double' | 'shadow' | 'curved' | ... other options supported by win open
+            winblend = 0,
+            highlights = {
+                border = 'Normal',
+                background = 'Normal',
+            },
+        },
+        winbar = {
+            enabled = true,
+            name_formatter = function(term) --  term: Terminal
+                return term.name
+            end,
+        },
+    })
 
-	local Terminal = require("toggleterm.terminal").Terminal
+    local Terminal = require('toggleterm.terminal').Terminal
 
-	-- local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
-	local lazygit = Terminal:new({
-		cmd = "lazygit",
-		dir = "git_dir",
-		direction = "float",
-		float_opts = {
-			border = "curved", -- 'single' | 'double' | 'shadow' | 'curved' | ... other options supported by win open
-		},
-		-- function to run on opening the terminal
-		on_open = function(term)
-			vim.cmd("startinsert!")
-			vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
-		end,
-		-- function to run on closing the terminal
-		-- on_close = function(term)
-		--   vim.cmd("Closing terminal")
-		-- end,
-	})
+    function _G.set_terminal_keymaps()
+        local opts = { buffer = 0 }
+        vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+        vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+        vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+        vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+        vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+        vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+        vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+    end
 
-	function _LAZYGIT_TOGGLE()
-		lazygit:toggle()
-	end
+    -- if you only want these mappings for toggle term use term://*toggleterm#* instead
+    vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
-	local node = Terminal:new({ cmd = "node", hidden = true })
+    local lazygit = Terminal:new({
+        cmd = 'lazygit',
+        dir = 'git_dir',
+        direction = 'float',
+        float_opts = {
+            border = 'double',
+        },
+        -- function to run on opening the terminal
+        on_open = function(term)
+            vim.cmd('startinsert!')
+            vim.api.nvim_buf_set_keymap(
+                term.bufnr,
+                'n',
+                'q',
+                '<cmd>close<CR>',
+                { noremap = true, silent = true }
+            )
+        end,
+        -- function to run on closing the terminal
+        on_close = function(term)
+            vim.cmd('startinsert!')
+        end,
+    })
 
-	function _NODE_TOGGLE()
-		node:toggle()
-	end
+    function _Lazygit_toggle()
+        lazygit:toggle()
+    end
 
-	local ncdu = Terminal:new({ cmd = "ncdu", hidden = true })
-
-	function _NCDU_TOGGLE()
-		ncdu:toggle()
-	end
-
-	local htop = Terminal:new({ cmd = "htop", hidden = true })
-
-	function _HTOP_TOGGLE()
-		htop:toggle()
-	end
-
-	local python = Terminal:new({ cmd = "python", hidden = true })
-
-	function _PYTHON_TOGGLE()
-		python:toggle()
-	end
-
-
+    -- vim.api.nvim_set_keymap(
+    --     'n',
+    --     '<leader>g',
+    --     '<cmd>lua _lazygit_toggle()<CR>',
+    --     { noremap = true, silent = true }
+    -- )
 end
 
 return M
