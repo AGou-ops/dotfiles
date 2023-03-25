7d5d9ac4d095c0699e82fca3f02792d86c397bde
AGou-ops <suofeiyaxx@gmail.com>
2023-03-25 15:14:18 +0800

    Add `autosave` autocmd.

--- neovim/lua/config/autocmds.lua
+++ neovim/lua/config/autocmds.lua
@@ -179,3 +179,17 @@ vim.api.nvim_create_autocmd('BufReadPost', {
         end
     end,
 })
+
+-- autosave file when buffer leave or focus lost
+vim.api.nvim_create_autocmd({ 'BufLeave', 'FocusLost' }, {
+    callback = function()
+        if
+            vim.bo.modified
+            and not vim.bo.readonly
+            and vim.fn.expand('%') ~= ''
+            and vim.bo.buftype == ''
+        then
+            vim.api.nvim_command('silent update')
+        end
+    end,
+})
