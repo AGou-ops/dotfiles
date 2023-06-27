34b59429c2bb8652acaea5594152b4a02f205d11
AGou-ops <suofeiyxx@gmail.com>
2023-06-27 09:24:35 +0800

    Disable netrw.

--- neovim/lua/config/options.lua
+++ neovim/lua/config/options.lua
@@ -171,3 +171,5 @@ vim.g.loaded_python_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_node_provider = 0
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrw = 1
