71e8d07f528ca9c6e4e60821cc046887e9512698
AGou-ops <suofeiyxx@gmail.com>
2023-06-29 15:17:43 +0800

    Change cmp sorting.

--- neovim/lua/plugins/cmp.lua
+++ neovim/lua/plugins/cmp.lua
@@ -43,7 +43,6 @@ function M.config()
             end
         end,
 
-        preselect = cmp.PreselectMode.Item,
         window = {
             completion = cmp.config.window.bordered(),
             documentation = cmp.config.window.bordered(),
@@ -61,15 +60,34 @@ function M.config()
             disallow_partial_matching = false,
             disallow_prefix_unmatching = true,
         },
+        preselect = cmp.PreselectMode.Item,
         sorting = {
             comparators = {
-                -- > To achieve consistency across languages and to honor different clients usually the client is responsible for filtering and sorting.
-                -- > This has also the advantage that client can experiment with different filter and sorting models.
-                -- > However servers can enforce different behavior by setting a sortText.
-                cmp.config.compare.sort_text,
-                -- > The score is matched char count generally.
+                -- cmp.config.compare.sort_text,
+                -- cmp.config.compare.score,
+                -- cmp.config.compare.recently_used,
+                cmp.config.compare.offset,
+                cmp.config.compare.exact,
                 cmp.config.compare.score,
-                cmp.config.compare.recently_used,
+
+                -- copied from cmp-under, but I don't think I need the plugin for this.
+                -- I might add some more of my own.
+                function(entry1, entry2)
+                    local _, entry1_under = entry1.completion_item.label:find('^_+')
+                    local _, entry2_under = entry2.completion_item.label:find('^_+')
+                    entry1_under = entry1_under or 0
+                    entry2_under = entry2_under or 0
+                    if entry1_under > entry2_under then
+                        return false
+                    elseif entry1_under < entry2_under then
+                        return true
+                    end
+                end,
+
+                cmp.config.compare.kind,
+                cmp.config.compare.sort_text,
+                cmp.config.compare.length,
+                cmp.config.compare.order,
             },
         },
         completion = {
