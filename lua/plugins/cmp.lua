90c32237db3a476efd0e8a8e46bd412ec1acb853
AGou-ops <suofeiyxx@gmail.com>
2023-07-04 15:55:48 +0800

    Update at 2023-07-04 15:55:46.

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -63,31 +63,31 @@ function M.config()
         preselect = cmp.PreselectMode.Item,
         sorting = {
             comparators = {
-                -- cmp.config.compare.sort_text,
-                -- cmp.config.compare.score,
-                -- cmp.config.compare.recently_used,
-                cmp.config.compare.offset,
-                cmp.config.compare.exact,
-                cmp.config.compare.score,
-
-                -- copied from cmp-under, but I don't think I need the plugin for this.
-                -- I might add some more of my own.
-                function(entry1, entry2)
-                    local _, entry1_under = entry1.completion_item.label:find('^_+')
-                    local _, entry2_under = entry2.completion_item.label:find('^_+')
-                    entry1_under = entry1_under or 0
-                    entry2_under = entry2_under or 0
-                    if entry1_under > entry2_under then
-                        return false
-                    elseif entry1_under < entry2_under then
-                        return true
-                    end
-                end,
-
-                cmp.config.compare.kind,
                 cmp.config.compare.sort_text,
-                cmp.config.compare.length,
-                cmp.config.compare.order,
+                cmp.config.compare.score,
+                cmp.config.compare.recently_used,
+                -- cmp.config.compare.offset,
+                -- cmp.config.compare.exact,
+                -- cmp.config.compare.score,
+                --
+                -- -- copied from cmp-under, but I don't think I need the plugin for this.
+                -- -- I might add some more of my own.
+                -- function(entry1, entry2)
+                --     local _, entry1_under = entry1.completion_item.label:find('^_+')
+                --     local _, entry2_under = entry2.completion_item.label:find('^_+')
+                --     entry1_under = entry1_under or 0
+                --     entry2_under = entry2_under or 0
+                --     if entry1_under > entry2_under then
+                --         return false
+                --     elseif entry1_under < entry2_under then
+                --         return true
+                --     end
+                -- end,
+                --
+                -- cmp.config.compare.kind,
+                -- cmp.config.compare.sort_text,
+                -- cmp.config.compare.length,
+                -- cmp.config.compare.order,
             },
         },
         completion = {
@@ -123,7 +123,7 @@ function M.config()
                     Operator = '',
                     TypeParameter = ' ',
                     Robot = '󱚤',
-                    Smiley = 'ﲃ',
+                    Smiley = '',
                     Note = ' ',
                 }
                 local meta_type = vim_item.kind
--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -1,16 +1,35 @@
 local M = {
     'nvimdev/guard.nvim',
     cmd = 'GuardFmt',
+    enabled = true,
     ft = { 'lua', 'go' },
     keys = { { '<leader>ef', '<cmd>GuardFmt<cr>', desc = 'Format current file.' } },
 }
 
 function M.config()
     local ft = require('guard.filetype')
+
+    -- use stylua to format lua files and no linter
     ft('lua'):fmt('stylua')
-    ft('go'):fmt('lsp'):append('golines')
-    --note setup must be last line
-    require('guard').setup()
+
+    -- use lsp to format first then use golines to format
+    ft('go'):fmt('goimports'):append('gofumpt')
+
+    -- call setup LAST
+    require('guard').setup({
+        -- the only option for the setup function
+        fmt_on_save = true,
+        goimports = {
+            cmd = 'goimports',
+            args = { '-' },
+            stdin = true,
+        },
+        gofumpt = {
+            cmd = 'gofumpt',
+            args = { '-' },
+            stdin = true,
+        },
+    })
 end
 
 return M
