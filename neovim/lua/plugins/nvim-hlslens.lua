34d301a9f9480ebda35b6aa54c6d0dfb7c52fc27
AGou-ops <suofeiyxx@gmail.com>
2023-07-11 10:06:09 +0800

    Remove useless keymaps.

--- neovim/lua/plugins/nvim-hlslens.lua
+++ neovim/lua/plugins/nvim-hlslens.lua
@@ -24,8 +24,6 @@ function M.config()
     vim.api.nvim_set_keymap('n', '#', [[#<Cmd>lua require('hlslens').start()<CR>]], kopts)
     vim.api.nvim_set_keymap('n', 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts)
     vim.api.nvim_set_keymap('n', 'g#', [[g#<Cmd>lua require('hlslens').start()<CR>]], kopts)
-
-    -- vim.api.nvim_set_keymap('n', '<Leader>l', '<Cmd>noh<CR>', kopts)
 end
 
 return M
