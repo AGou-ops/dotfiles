f4853ed317116c3c353542c11dd9e9750fe3fc43
AGou-ops <suofeiyxx@gmail.com>
2023-07-11 10:07:03 +0800

    Change OPTIMIZE icon.

--- neovim/lua/plugins/todo-comments.lua
+++ neovim/lua/plugins/todo-comments.lua
@@ -21,7 +21,7 @@ function M.config()
             TODO = { icon = ' ', color = 'info' },
             HACK = { icon = ' ', color = 'warning' },
             WARN = { icon = ' ', color = 'warning', alt = { 'WARNING', 'XXX' } },
-            PERF = { icon = ' ', alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
+            PERF = { icon = ' ', alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
             NOTE = { icon = ' ', colr = 'hint', alt = { 'INFO' } },
         },
         merge_keywords = true, -- when true, custom keywords will be merged with the defaults
