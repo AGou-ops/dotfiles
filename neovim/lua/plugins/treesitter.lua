c16502380ea52e828bfb110abc7992f2ab968318
AGou-ops <suofeiyxx@gmail.com>
2023-08-31 09:20:22 +0800

    Disable ts playground.

--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -188,7 +188,7 @@ return {
                     },
                 },
                 playground = {
-                    enable = true,
+                    enable = false,
                     disable = {},
                     updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
                     persist_queries = true, -- Whether the query persists across vim sessions
