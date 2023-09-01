32aeed1f159231bb5f829d048c24d5415cd65b4d
AGou-ops <suofeiyxx@gmail.com>
2023-09-01 15:02:10 +0800

    Reset nvim-surround config.

--- neovim/lua/plugins/nvim-surround.lua
+++ neovim/lua/plugins/nvim-surround.lua
@@ -1,6 +1,5 @@
 local M = {
     'kylechui/nvim-surround',
-    -- lazy = false,
     event = 'VeryLazy',
 }
 
@@ -8,18 +7,18 @@ function M.config()
     require('nvim-surround').setup({
         -- Configuration here, or leave empty to use defaults
         keymaps = {
-            insert = '<C-g>s',
-            insert_line = '<C-g>S',
-            normal = 'ys',
-            normal_cur = 'yss',
-            normal_line = 'yS',
-            normal_cur_line = 'ySS',
             visual = 'Q',
-            visual_line = 'gS',
-            delete = 'ds',
-            change = 'cs',
         },
     })
+    -- 	    Old text                    Command         New text
+    -- --------------------------------------------------------------------------------
+    --     surr*ound_words             ysiw)           (surround_words)
+    --     *make strings               ys$"            "make strings"
+    --     [delete ar*ound me!]        ds]             delete around me!
+    --     remove <b>HTML t*ags</b>    dst             remove HTML tags
+    --     'change quot*es'            cs'"            "change quotes"
+    --     <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
+    --     delete(functi*on calls)     dsf             function calls
 
     -- sample Text
     --
