f7bbfaf2304f97524c16dea91521f14a66a56bd7
AGou-ops <suofeiyxx@gmail.com>
2023-09-14 09:36:06 +0800

    Update at 2023-09-14 09:36:05.

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -140,17 +140,7 @@ function M.config()
             respect_root = false,
             color_mode = true,
         },
-        outline = {
-            win_position = 'right',
-            win_with = '',
-            win_width = 30,
-            show_detail = true,
-            auto_preview = true,
-            auto_refresh = true,
-            auto_close = true,
-            custom_sort = nil,
-            keys = { jump = 'o', expand_collapse = 'u', quit = 'q' },
-        },
+        outline = {},
         callhierarchy = {
             show_detail = false,
             keys = {
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -40,13 +40,12 @@ function M.config()
 
     function _G.set_terminal_keymaps()
         local opts = { buffer = 0 }
-        vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-        vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+        -- vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
         -- vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-        -- vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-        -- vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+        vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+        vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
         -- vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-        -- vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+        vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
     end
 
     -- if you only want these mappings for toggle term use term://*toggleterm#* instead
@@ -79,13 +78,6 @@ function M.config()
     function _Lazygit_toggle()
         lazygit:toggle()
     end
-
-    -- vim.api.nvim_set_keymap(
-    --     'n',
-    --     '<leader>g',
-    --     '<cmd>lua _lazygit_toggle()<CR>',
-    --     { noremap = true, silent = true }
-    -- )
 end
 
 return M
