e0e323993a28d89146f358e93aec004b1348f36d
AGou-ops <suofeiyxx@gmail.com>
2024-01-31 14:18:41 +0800

    Update at 2024-01-31 14:18:40.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,14 +8,14 @@ local opts = { noremap = true, silent = true }
 -- vim.api.nvim_create_user_command('TransparentToggle', 'hi Normal ctermbg=NONE guibg=NONE | hi EndOfBuffer guibg=NONE ctermbg=NONE', {})
 -- bind('n', '<F3>', '<CMD>TransparentToggle<CR>')
 
-bind('n', ';', ':')
-bind('n', 'U', '<C-r>')
-bind('v', ';', ':')
+bind('n', ';', ':', opts)
+bind('n', 'U', '<C-r>', opts)
+bind('v', ';', ':', opts)
 
-bind('c', 'W', 'w')
-bind('c', 'WQ', 'wq')
-bind('c', 'Wq', 'wq')
-bind('c', 'QA', 'qa')
+bind('c', 'W', 'w', opts)
+bind('c', 'WQ', 'wq', opts)
+bind('c', 'Wq', 'wq', opts)
+bind('c', 'QA', 'qa', opts)
 
 -- do not copy delete and change command
 bind('n', 'd', [["_d]], opts)
@@ -26,113 +26,115 @@ bind('v', 'c', [["_c]], opts)
 bind('n', '<C-o>', '<C-o>zz', opts)
 bind('n', '<C-i>', '<C-i>zz', opts)
 
-bind('n', 'q', '<Nop>')
-bind('n', 'H', 'g^')
-bind('n', 'L', 'g_')
-bind('v', 'H', 'g^')
-bind('v', 'L', 'g_')
-bind('n', 'k', 'gk')
-bind('n', 'j', 'gj')
-bind('v', '<tab>', '<S->>gv')
-bind('v', '<S-tab>', '<S-<>gv')
-bind('n', '<C-left>', '<CMD>bp<CR>')
-bind('n', '<C-right>', '<CMD>bn<CR>')
+bind('n', 'q', '<Nop>', opts)
+bind('n', 'H', 'g^', opts)
+bind('n', 'L', 'g_', opts)
+bind('v', 'H', 'g^', opts)
+bind('v', 'L', 'g_', opts)
+bind('n', 'k', 'gk', opts)
+bind('n', 'j', 'gj', opts)
+bind('v', '<tab>', '<S->>gv', opts)
+bind('v', '<S-tab>', '<S-<>gv', opts)
+bind('n', '<C-left>', '<CMD>bp<CR>', opts)
+bind('n', '<C-right>', '<CMD>bn<CR>', opts)
 
-bind('n', '<C-t>', '<CMD>tabnew<CR>')
-bind('n', '<C-t>', '<Esc><CMD>tabnew<CR>')
+bind('n', '<C-t>', '<CMD>tabnew<CR>', opts)
+bind('n', '<C-t>', '<Esc><CMD>tabnew<CR>', opts)
 
-bind('n', '<ESC>', '<CMD>noh<CR>')
+bind('n', '<ESC>', '<CMD>noh<CR>', opts)
 
-bind('n', '<leader>0', '<CMD>tablast<CR>')
-bind('n', '<leader>dd', ':%bdelete<CR>')
+bind('n', '<leader>0', '<CMD>tablast<CR>', opts)
+bind('n', '<leader>dd', ':%bdelete<CR>', opts)
 
 -- copy to system clipboard
-bind('n', '<C-c>', '"+y')
-bind('n', '<C-h>', '<C-w>h')
-bind('n', '<C-j>', '<C-w>j')
-bind('n', '<C-k>', '<C-w>k')
-bind('n', '<C-l>', '<C-w>l')
-bind('n', '<C-q>', '<C-w>q')
-bind('n', '<C-\\>', '<C-w><bar>')
+bind('n', '<C-c>', '"+y', opts)
+bind('n', '<C-h>', '<C-w>h', opts)
+bind('n', '<C-j>', '<C-w>j', opts)
+bind('n', '<C-k>', '<C-w>k', opts)
+bind('n', '<C-l>', '<C-w>l', opts)
+bind('n', '<C-q>', '<C-w>q', opts)
+bind('n', '<C-\\>', '<C-w><bar>', opts)
 
 -- Auto indent pasted text
--- bind('n', 'p', ']p')
--- bind('n', 'P', '[p')
-bind('n', '(', '%')
-bind('n', ')', '%')
+-- bind('n', 'p', ']p', opts)
+-- bind('n', 'P', '[p', opts)
+bind('n', '(', '%', opts)
+bind('n', ', opts)', '%', opts)
 
 -- turn off direction keyboard, force yourself use `hjkl` !!!
-bind('n', '<Left>', '<Nop>')
-bind('n', '<Right>', '<Nop>')
-bind('n', '<Up>', '<Nop>')
-bind('n', '<Down>', '<Nop>')
-bind('n', '<space>', '/\\v')
-bind('t', '<Esc>', '<C-\\><C-n>')
+bind('n', '<Left>', '<Nop>', opts)
+bind('n', '<Right>', '<Nop>', opts)
+bind('n', '<Up>', '<Nop>', opts)
+bind('n', '<Down>', '<Nop>', opts)
+bind('n', '<space>', '/\\v', opts)
+bind('t', '<Esc>', '<C-\\><C-n>', opts)
 
 -- tab quick swich
-bind('n', '<leader>th', '<CMD>tabfirst<CR>')
-bind('n', '<leader>tl', '<CMD>tablast<CR>')
-bind('n', '<leader>tj', '<CMD>tabnext<CR>')
-bind('n', '<leader>tk', '<CMD>tabprev<CR>')
-bind('n', '<leader>tn', '<CMD>tabnext<CR>')
--- bind('n', '<leader>tp', '<CMD>tabprev<CR>')
-bind('n', '<leader>te', '<CMD>tabedit<CR>')
--- bind('n', '<leader>td', '<CMD>tabclose<CR>')
-bind('n', '<leader>tm', '<CMD>tabm<CR>')
+bind('n', '<leader>th', '<CMD>tabfirst<CR>', opts)
+bind('n', '<leader>tl', '<CMD>tablast<CR>', opts)
+bind('n', '<leader>tj', '<CMD>tabnext<CR>', opts)
+bind('n', '<leader>tk', '<CMD>tabprev<CR>', opts)
+bind('n', '<leader>tn', '<CMD>tabnext<CR>', opts)
+-- bind('n', '<leader>tp', '<CMD>tabprev<CR>', opts)
+bind('n', '<leader>te', '<CMD>tabedit<CR>', opts)
+-- bind('n', '<leader>td', '<CMD>tabclose<CR>', opts)
+bind('n', '<leader>tm', '<CMD>tabm<CR>', opts)
 
 -- ============= Plugin Keymaps =============
--- git open curren repo
+-- git open current repo
 bind('n', '<leader>go', '<CMD>:!git open<CR><CR>', opts)
 -- toggleTerm & Lazygit
-bind('n', '<leader>tf', '<CMD>exe v:count1 . "ToggleTerm"<CR>')
-bind('n', '<leader>tb', '<CMD>ToggleTerm size=12 direction=horizontal<CR>')
-bind('n', '<leader>tt', '<CMD>ToggleTerm direction=tab<CR>')
-bind('n', '<leader>gg', '<CMD>lua _Lazygit_toggle()<CR>')
+bind('n', '<leader>tf', '<CMD>exe v:count1 . "ToggleTerm"<CR>', opts)
+bind('n', '<leader>tb', '<CMD>ToggleTerm size=12 direction=horizontal<CR>', opts)
+bind('n', '<leader>tt', '<CMD>ToggleTerm direction=tab<CR>', opts)
+bind('n', '<leader>gg', '<CMD>lua _Lazygit_toggle()<CR>', opts)
 
 -- vim-translator
--- bind('n', '<leader>ts', '<CMD>TranslateW<CR>')
+-- bind('n', '<leader>ts', '<CMD>TranslateW<CR>', opts)
 --
 
 -- treesitter
-bind('n', '<leader>hd', '<CMD>TSDisable highlight<CR>')
-bind('n', '<leader>he', '<CMD>TSEnable highlight<CR>')
+bind('n', '<leader>hd', '<CMD>TSDisable highlight<CR>', opts)
+bind('n', '<leader>he', '<CMD>TSEnable highlight<CR>', opts)
 
 -- leaderF
-bind('n', '<leader>lg', '<CMD>LeaderfRgInteractive<CR>')
-bind('n', '<leader>lf', '<CMD>LeaderfFile<CR>')
-bind('n', '<leader>lF', '<CMD>LeaderfFunctionAll<CR>')
+bind('n', '<leader>lg', '<CMD>LeaderfRgInteractive<CR>', opts)
+bind('n', '<leader>lf', '<CMD>LeaderfFile<CR>', opts)
+bind('n', '<leader>lF', '<CMD>LeaderfFunctionAll<CR>', opts)
 
 -- telescope-dap
-bind('n', '<leader>dcc', '<CMD>Telescope dap commands<CR>')
-bind('n', '<leader>dcf', '<CMD>Telescope dap configurations<CR>')
-bind('n', '<leader>dl', '<CMD>Telescope dap list_breakpoints<CR>')
-bind('n', '<leader>dv', '<CMD>Telescope dap variables<CR>')
-bind('n', '<leader>df', '<CMD>Telescope dap frames<CR>')
+bind('n', '<leader>dcc', '<CMD>Telescope dap commands<CR>', opts)
+bind('n', '<leader>dcf', '<CMD>Telescope dap configurations<CR>', opts)
+bind('n', '<leader>dl', '<CMD>Telescope dap list_breakpoints<CR>', opts)
+bind('n', '<leader>dv', '<CMD>Telescope dap variables<CR>', opts)
+bind('n', '<leader>df', '<CMD>Telescope dap frames<CR>', opts)
 -- dap
-bind('n', '<leader>bp', '<CMD>DapToggleBreakpoint<CR>')
-bind('n', '<leader>ds', '<CMD>DapTerminate<CR>')
-bind('n', '<leader>dt', '<CMD>DapTerminate<CR>')
-bind('n', '<leader>db', '<CMD>DapContinue<CR>')
-bind('n', '<leader>du', '<CMD>lua require("dapui").toggle()<CR>')
+bind('n', '<leader>bp', '<CMD>DapToggleBreakpoint<CR>', opts)
+bind('n', '<leader>ds', '<CMD>DapTerminate<CR>', opts)
+bind('n', '<leader>dt', '<CMD>DapTerminate<CR>', opts)
+bind('n', '<leader>db', '<CMD>DapContinue<CR>', opts)
+bind('n', '<leader>du', '<CMD>lua require("dapui").toggle()<CR>', opts)
 
 -- -- undotree
-bind('n', '<leader>oh', ':UndotreeToggle <BAR> :UndotreeFocus<CR>')
+bind('n', '<leader>oh', ':UndotreeToggle <BAR> :UndotreeFocus<CR>', opts)
 
 -- -- tagbar
--- bind('n', '<leader>tb', ':TagbarToggle<CR>')
+-- bind('n', '<leader>tb', ':TagbarToggle<CR>', opts)
 
 -- plenary debug
 bind(
     'n',
     '<leader>nd',
-    ':lua require("plenary.profile").start("profile.log", { flame = true })<CR>'
+    ':lua require("plenary.profile").start("profile.log", { flame = true })<CR>',
+    opts
 )
 bind(
     'n',
     '<leader>ns',
-    ':lua require("plenary.profile").stop()<CR> | :!flamegraph.pl profile.log > flame.svg && rm -f profile.log && open flame.svg<CR>'
+    ':lua require("plenary.profile").stop()<CR> | :!flamegraph.pl profile.log > flame.svg && rm -f profile.log && open flame.svg<CR>',
+    opts
 )
 --
 -- close lspinlayHit
-bind('n', '<leader>io', '<CMD>lua vim.lsp.inlay_hint(0, true)<CR>')
-bind('n', '<leader>ic', '<CMD>lua vim.lsp.inlay_hint(0, false)<CR>')
+bind('n', '<leader>io', '<CMD>lua vim.lsp.inlay_hint(0, true)<CR>', opts)
+bind('n', '<leader>ic', '<CMD>lua vim.lsp.inlay_hint(0, false)<CR>', opts)
