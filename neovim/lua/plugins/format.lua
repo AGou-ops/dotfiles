3114018954caf03890261356bcd4558e91f0541a
AGou-ops <suofeiyaxx@gmail.com>
2024-02-21 16:55:25 +0800

    Async format.

--- neovim/lua/plugins/format.lua
+++ neovim/lua/plugins/format.lua
@@ -7,10 +7,10 @@ local M = {
 
 function M.config()
     require('conform').setup({
-        format_on_save = {
-            lsp_fallback = true,
-            timeout_ms = 500,
-        },
+        -- format_on_save = {
+        --     lsp_fallback = true,
+        --     timeout_ms = 500,
+        -- },
         format_after_save = {
             lsp_fallback = true,
         },
@@ -20,7 +20,7 @@ function M.config()
         formatters_by_ft = {
             lua = { 'stylua' },
             -- Conform will run multiple formatters sequentially
-            go = { 'goimports', 'gofmt' },
+            go = { 'goimports', 'gofumpt' },
             -- Use a sub-list to run only the first available formatter
             javascript = { { 'prettierd', 'prettier' } },
             -- You can use a function here to determine the formatters dynamically
