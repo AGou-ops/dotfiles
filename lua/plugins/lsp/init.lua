43bcf621901656e0bfe4a4c3084014aaa414245e
AGou-ops <suofeiyxx@gmail.com>
2025-09-28 14:31:04 +0800

    Update at 2025-09-28 14:31:02.

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -159,4 +159,10 @@ for server_name, lsp_executable in pairs(enabled_lsp_servers) do
     end
 end
 
+local signs = { Error = "✘ ", Warn = " ", Hint = "⚑ ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
+
 return M
